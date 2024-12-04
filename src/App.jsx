e21d79ed48c1280aa18c93595e26bd21ce07b576
Anubhab <anubhab362@gmail.com>
2024-12-04 12:43:13 +0530

    using GA through react-ga4 package

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { RouterProvider } from "@tanstack/react-router";
 import router from "../src/router/index";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-2Q5N5DFX1X");
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname,
+      title: "App.jsx",
+    });
+  }, []);
   return <RouterProvider router={router} />;
 }
 
