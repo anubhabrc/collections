55bc4132372d547650d96e3023c2bf8c2be874e5
Anubhab <anubhab362@gmail.com>
2024-12-31 22:41:29 +0530

    posthog analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+  capture_pageview: true, // Automatically capture pageviews
+  disable_session_recording: false, // Enable session recordings
+  autocapture: true,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </StrictMode>
 );
